2065c1785057a4eee650035ce48f6f29a1ef05df
Kukaass <chestermaligaso29@gmail.com>
2024-05-19 14:14:52 +0800

    inventory endpoint done

--- src/inventory/inventory.router.ts
+++ src/inventory/inventory.router.ts
@@ -27,3 +27,89 @@ inventoryRouter.post(
         }
     }
 );
+
+//ADD to inventory
+inventoryRouter.post(
+    '/',
+    body('type').isString().notEmpty(),
+    body('quantity').isInt(),
+    async (req: Request, res: Response) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.status(400).json({ errors: errors.array() });
+            return;
+        }
+
+        const { type, quantity } = req.body;
+        try {
+            const inventory = await inventoryServer.createInventoryData({
+                type,
+                quantity
+            });
+            res.json(inventory);
+        } catch (error) {
+            res.status(500).json({ error: 'Error adding to inventory' });
+        }
+    }
+);
+
+//GET all inventory data
+inventoryRouter.get('/', async (request: Request, response: Response) => {
+    try {
+        const inventoryData = await inventoryServer.getInventoryData()
+        response.json(inventoryData)
+    } catch (error) {
+        response.status(500).json({ error, message: 'Error fetching Inventory' });
+    }
+})
+
+//GET inventory data by id
+inventoryRouter.get('/:id', async (request: Request, response: Response) => {
+    try {
+        const inventoryData = await inventoryServer.getInventoryDataById(request.params.id);
+        if (!inventoryData) {
+            response.status(404).json({ error: 'User not found' });
+            return;
+        }
+        response.json(inventoryData);
+    } catch (error) {
+        response.status(500).json({ error: 'Error fetching user by ID' });
+    }
+});
+
+//UPDATE inventory data
+inventoryRouter.put(
+    '/:id',
+    body('type').isString().notEmpty(),
+    body('quantity').isInt(),
+    async (request: Request, response: Response) => {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+            response.status(400).json({ errors: errors.array() });
+            return;
+        }
+
+        const { id } = request.params
+
+        try {
+            const inventory = await inventoryServer.updateInventoryData(id, request.body);
+            response.json({ inventory, message: 'User updated successfully' });
+        } catch (error) {
+            
+        }
+    }
+)
+
+//DELETE inventory Data
+inventoryRouter.delete('/:id', async (request: Request, response: Response) => {
+    try {
+        const inventoryData = await inventoryServer.deleteInventoryData(request.params.id);
+        if (!inventoryData) {
+            response.status(404).json({ error: 'Data not found' });
+            return;
+        }
+        response.json({ inventoryData, message: 'Data deleted successfully' });
+    } catch (error) {
+        response.status(500).json({ error: 'Error deleting data' });
+    }
+});
--- src/inventory/inventory.server.ts
+++ src/inventory/inventory.server.ts
@@ -1,6 +1,9 @@
+import {  Prisma } from '@prisma/client';
 import { prisma } from '../utils/db.server';
+import { response } from 'express';
 
 export type Inventory = {
+    id: string,
     type: string;
     quantity: number;
 }
@@ -50,3 +53,95 @@ export const removeFromInventory = async (type: string, quantity: number): Promi
     }
 };
 
+//ADD to inventory
+export const createInventoryData = async (inventoryData: Prisma.InventoryDataCreateInput): Promise<Inventory> => {
+    try {
+        const inventory = await prisma.inventoryData.create({
+            data: inventoryData,
+            select: {
+                id: true,
+                type: true,
+                quantity: true
+            }
+        });
+        return inventory;
+    } catch (error) {
+        console.error('Error adding to inventory:', error);
+        throw error;
+    }
+};
+
+
+//GET all items in the inventory
+export const getInventoryData = async(): Promise<Inventory[]> => {
+    try {
+        return await prisma.inventoryData.findMany({
+            select: {
+                id: true,
+                type: true,
+                quantity: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        })
+    } catch (error) {
+        response.status(500).json({error, message: 'Error fetching Inventory'})
+        throw error;
+    }
+}
+
+//GET inventory by id
+export const getInventoryDataById = async (id: string): Promise<Inventory | null> => {
+    try {
+        return await prisma.inventoryData.findUnique({
+            where: { id },
+            select: {
+                id: true,
+                type: true,
+                quantity: true,
+                createdAt: true,
+                updatedAt: true
+            }
+        })
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+//UPDATE inventory data
+export const updateInventoryData = async (id: string, inventory: Partial<Inventory>): Promise<Inventory> => {
+    try {
+        return await prisma.inventoryData.update({
+            where: { id },
+            data: inventory,
+            select: {
+                id: true,
+                type: true,
+                quantity: true,
+                createdAt: true,
+                updatedAt: true
+            },
+        });
+    } catch (error) {
+        console.error('Error updating inventory:', error);
+        throw error;
+    }
+};
+
+//DELETE inventory by id
+export const deleteInventoryData = async (id: string): Promise<Inventory> => {
+    try {
+        return await prisma.inventoryData.delete({
+            where: { id },
+            select: {
+                id: true,
+                type: true,
+                quantity: true,
+            },
+        });
+    } catch (error) {
+        console.error('Error deleting user:', error);
+        throw error;
+    }
+};
