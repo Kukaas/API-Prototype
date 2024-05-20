883908e8e7509c3de8434bd3c2b539f388bdfa76
Kukaass <chestermaligaso29@gmail.com>
2024-05-20 21:19:42 +0800

    inayos update user

--- src/user/user.router.ts
+++ src/user/user.router.ts
@@ -97,7 +97,7 @@ userRouter.put(
 
         const { id } = request.params;
         const { name, email } = request.body;
-        const birthDate = new Date(request.body.birthDate).toISOString().split('T')[0];
+        const birthDate = new Date(request.body.birthDate).toISOString();
 
         // Check if a user with the same email or name already exists
         const existingUser = await prisma.user.findFirst({
